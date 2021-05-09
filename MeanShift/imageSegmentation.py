41c8b605c3ad8d9b189df220d2c5bdea5ab49211
timeeehd <timeeehd@gmail.com>
2021-05-09 22:11:03 +0200

    Added the optimizations with some images
    Images will still be deleted to make sure :)

--- MeanShift/imageSegmentation.py
+++ MeanShift/imageSegmentation.py
@@ -8,6 +8,7 @@ from plotclusters3D import *
 
 from time import time
 
+
 # def distance(point1, point2):
 #     distance =
 
@@ -138,27 +139,27 @@ def new_meanshift(data, r):
             labels[points[:]] = new_label
             peak_values[points[:]] = new_peak
 
-
-        peakMovements = np.sum(np.abs(old_peak_values- peak_values))
+        peakMovements = np.sum(np.abs(old_peak_values - peak_values))
         print(f"Run {runs} with peakmovements: {peakMovements}")
         if peakMovements < t:
             optimal = True
     return labels, peaks
 
 
-def find_peak(data, idx, r):
+def find_peak(data, idx, r, t=0.01):
     distances = cdist(data[idx].reshape(1, -1), data, metric='euclidean')
     points = np.argwhere(distances <= r)[:, 1]
     peak = np.mean(data[points[:]], axis=0)
     distance = 1000
-    while distance > 0.01:
+    while distance > t:
         old_peak = peak
-        distances = cdist(old_peak.reshape(1,-1), data, metric='euclidean')
+        distances = cdist(old_peak.reshape(1, -1), data, metric='euclidean')
         points = np.argwhere(distances <= r)[:, 1]
         peak = np.mean(data[points[:]], axis=0)
-        distance = np.linalg.norm(old_peak-peak)
+        distance = np.linalg.norm(old_peak - peak)
 
     return peak
+
 
 def mean_shift(data, r):
     labels = np.zeros(len(data))
@@ -167,10 +168,10 @@ def mean_shift(data, r):
     label_peak = {}
     peak_values = np.copy(data)
     for i, point in enumerate(tqdm.tqdm(data)):
-        peak = find_peak(data, i , r)
+        peak = find_peak(data, i, r)
         if len(peaks) != 0:
             peakDistance = cdist(peak.reshape(1, -1), np.array(peaks))
-            points = np.argwhere(peakDistance < r / 2)[:,1]
+            points = np.argwhere(peakDistance < r / 2)[:, 1]
             if len(points) != 0:
                 labels[i] = points[0]
                 peak_values[i] = label_peak[points[0]]
@@ -186,26 +187,116 @@ def mean_shift(data, r):
             label_peak[number_of_labels] = peak
             peak_values[i] = peak
             number_of_labels += 1
-    return labels, np.array(peaks), peak_values
+    return labels, np.array(peaks)
 
 
+def find_peak_opt(data, idx, r, t=0.01, c=4):
+    cpts = np.zeros(len(data))
+    distances = cdist(data[idx].reshape(1, -1), data, metric='euclidean')
+    points = np.argwhere(distances <= r)[:, 1]
+    peak = np.mean(data[points[:]], axis=0)
+    search_path_distance = cdist(peak.reshape(1, -1), data, metric='euclidean')
+    search_path_points = np.argwhere(search_path_distance <= r / c)[:, 1]
+    cpts[search_path_points[:]] = 1
+    distance = 1000
+    while distance > t:
+        old_peak = peak
+        distances = cdist(old_peak.reshape(1, -1), data, metric='euclidean')
+        points = np.argwhere(distances <= r)[:, 1]
+        peak = np.mean(data[points[:]], axis=0)
+        search_path_distance = cdist(peak.reshape(1, -1), data, metric='euclidean')
+        search_path_points = np.argwhere(search_path_distance <= r / c)[:, 1]
+        cpts[search_path_points[:]] = 1
+        distance = np.linalg.norm(old_peak - peak)
+    return peak, cpts
 
 
-def imSegment(im, r, basin_of_attraction=True):
+def mean_shift_opt(data, r, threshold, c, basin=False, path=False):
+    labels = np.full(len(data), -1)
+    peaks = []
+    number_of_labels = 0
+    label_peak = {}
+    peak_values = np.copy(data)
+    for i, point in enumerate(tqdm.tqdm(data)):
+        if labels[i] == -1:
+            if path:
+                peak, cpts = find_peak_opt(data, i, r, threshold, c)
+                search_path = np.argwhere(cpts == 1)
+                if len(peaks) != 0:
+                    peakDistance = cdist(peak.reshape(1, -1), np.array(peaks))
+                    points = np.argwhere(peakDistance < r / 2)[:, 1]
+                    if len(points) != 0:
+                        labels[i] = points[0]
+                        peak_values[i] = label_peak[points[0]]
+                        labels[search_path[:]] = points[0]
+                    else:
+                        peaks.append(peak)
+                        if basin:
+                            distances = cdist(peak.reshape(1, -1), data, metric='euclidean')
+                            points = np.argwhere(distances <= r)[:, 1]
+                            labels[points[:]] = number_of_labels
+                        labels[search_path[:]] = number_of_labels
+                        labels[i] = number_of_labels
+                        label_peak[number_of_labels] = peak
+                        peak_values[i] = peak
+                        number_of_labels += 1
+                else:
+                    peaks.append(peak)
+                    if basin:
+                        distances = cdist(peak.reshape(1, -1), data, metric='euclidean')
+                        points = np.argwhere(distances <= r)[:, 1]
+                        labels[points[:]] = number_of_labels
+                    labels[search_path[:]] = number_of_labels
+                    labels[i] = number_of_labels
+                    label_peak[number_of_labels] = peak
+                    peak_values[i] = peak
+                    number_of_labels += 1
+            else:
+                peak = find_peak(data, i, r)
+                if len(peaks) != 0:
+                    peakDistance = cdist(peak.reshape(1, -1), np.array(peaks))
+                    points = np.argwhere(peakDistance < r / 2)[:, 1]
+                    if len(points) != 0:
+                        labels[i] = points[0]
+                        peak_values[i] = label_peak[points[0]]
+                    else:
+                        peaks.append(peak)
+                        if basin:
+                            distances = cdist(peak.reshape(1, -1), data, metric='euclidean')
+                            points = np.argwhere(distances <= r)[:, 1]
+                            labels[points[:]] = number_of_labels
+                        labels[i] = number_of_labels
+                        label_peak[number_of_labels] = peak
+                        peak_values[i] = peak
+                        number_of_labels += 1
+                else:
+                    peaks.append(peak)
+                    if basin:
+                        distances = cdist(peak.reshape(1, -1), data, metric='euclidean')
+                        points = np.argwhere(distances <= r)[:, 1]
+                        labels[points[:]] = number_of_labels
+                    labels[i] = number_of_labels
+                    label_peak[number_of_labels] = peak
+                    peak_values[i] = peak
+                    number_of_labels += 1
+    return labels, np.array(peaks)
+
+
+def imSegment(im, r, threshold=0.01, c=4, basin_of_attraction=True, path_speedup=True):
     imlab = rgb2lab(im)
     imlab = imlab.reshape(-1, 3)
-    segemented_values2 = np.zeros(len(imlab))
-    labels, peaks, segemented_values = mean_shift(imlab, r)
-    segemented_values2 = np.zeros((len(labels), 3))
-    for idx,x in enumerate(peaks):
-        points = np.argwhere(labels == idx)[:,0]
-        segemented_values2[points[:]] = x
+    if basin_of_attraction or path_speedup:
+        labels, peaks = mean_shift_opt(imlab, r, threshold, c, basin_of_attraction, path_speedup)
+    else:
+        labels, peaks = mean_shift(imlab, r)
+    segemented_values = np.zeros((len(labels), 3))
+    for idx, x in enumerate(peaks):
+        points = np.argwhere(labels == idx)[:, 0]
+        segemented_values[points[:]] = x
+    print(f'number of unique labels {np.unique(labels)}')
     segmented_image = segemented_values.reshape(im.shape)
-    segmented_image2 = segemented_values2.reshape(im.shape)
-    # print(segmented_image == segmented_image2)
     segmented_image_rgb = lab2rgb(segmented_image)
-    segmented_image2_rgb = lab2rgb(segmented_image2)
-    return segmented_image_rgb, labels, segmented_image2_rgb
+    return segmented_image_rgb, labels
 
 
 if __name__ == '__main__':
@@ -217,23 +308,44 @@ if __name__ == '__main__':
     # print(np.unique(np.array(labels)))
     # plotclusters3D(data.T, labels, np.array(peaks), 'Images/databaseClustered.png')
 
-    picture = "bulbasaur"
+    picture = "55075"
     image = plt.imread(f"./Data/{picture}.jpg")
 
-
-    r = 50
+    # r = 20
+    # start_time = time()
+    # segmented_image_3d, labels_3d = imSegment(image, r, basin_of_attraction=False, path_speedup=False)
+    # print(f"Took {time() - start_time} seconds for 3D")
+    # plt.title(f"Mean-shift 3D with r={r}")
+    # plt.imshow(segmented_image_3d)
+    # plt.savefig(f'Images/{picture} Mean-shift 3D with r={r}')
+    # plt.show()
+    r = 5
     start_time = time()
-    segmented_image_3d, labels_3d, segmented_image2_3d = imSegment(image, r, True)
+    segmented_image_3d, labels_3d = imSegment(image, r, basin_of_attraction=True, path_speedup=False)
     print(f"Took {time() - start_time} seconds for 3D")
     plt.title(f"Mean-shift 3D with r={r}")
     plt.imshow(segmented_image_3d)
-    plt.savefig(f'Images/{picture} Mean-shift 3D with r={r}')
-    plt.show()
-    plt.title(f"Mean-shift 3D with r={r}")
-    plt.imshow(segmented_image2_3d)
     plt.savefig(f'Images/{picture} Mean-shift 3D with r={r} 2')
-    plt.show()
-    # r = 20
+    # plt.show()
+
+    r = 5
+    start_time = time()
+    segmented_image_3d, labels_3d = imSegment(image, r, basin_of_attraction=False, path_speedup=True)
+    print(f"Took {time() - start_time} seconds for 3D")
+    plt.title(f"Mean-shift 3D with r={r}")
+    plt.imshow(segmented_image_3d)
+    plt.savefig(f'Images/{picture} Mean-shift 3D with r={r} 3')
+    # plt.show()
+    #
+    r = 5
+    start_time = time()
+    segmented_image_3d, labels_3d = imSegment(image, r, basin_of_attraction=True, path_speedup=True)
+    print(f"Took {time() - start_time} seconds for 3D")
+    plt.title(f"Mean-shift 3D with r={r}")
+    plt.imshow(segmented_image_3d)
+    plt.savefig(f'Images/{picture} Mean-shift 3D with r={r} 4')
+    # plt.show()
+    r = 20
     # start_time = time()
     # segmented_image_3d, labels_3d = imSegment(image, r, False)
     # print(f"Took {time() - start_time} seconds for 3D")
